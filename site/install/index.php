87478d4d0ba1b395b704a3dd332bff02198accc0
Paragramex <kadinjames77@gmail.com>
2022-10-17 18:28:19 -0700

    added change default admin account

--- site/install/index.php
+++ site/install/index.php
@@ -15,8 +15,11 @@
 	<p>Set up your site here:</p>
 	<form action="/../site/install/install.php" method="post">
 	<label for="title">Type the title of the site:</label>
-	<input type="text" id="title" name="title" required="required" value="My Company :)" size="35"/>
+	<input type="text" id="title" name="title" required="required" value="Site-Template" size="35">
 	<br />
 	Dont put html, will be used for site title and headers that include site name.
+	<br>
+	<label for="admins">Type the admin account (or leave as is for default admin account):</label>
+	<input type="text" id="admins" name="admins" required="required" value="admin" size="30">
 	<hr />
-	<input type="submit" value="install" /></form></bodybody></html>
+	<input type="submit" value="install"></input></form></bodybody></html>
--- site/install/install.php
+++ site/install/install.php
@@ -10,12 +10,14 @@
 			header('Location: ..');
 		}
 		$title = $_POST['title'];
+		$admins = $_POST['admins'];
 		class Installation {
 			public $sitetitle = "default";
+			public $admins = "admin";
 		}
 		$install = new Installation;
 		$install->sitetitle = $title;
-		$install->admins = array('admin');
+		$install->admins = $admins;
 		$json = json_encode($install);
 		$handle = fopen(__DIR__ . "/../../config.json", 'w+');
 		$status = fwrite($handle, $json);
